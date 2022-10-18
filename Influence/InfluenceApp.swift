89a2519b0ac2c3827eadb245218eb1a08db82cf9
TedWu716 <ted8322756@gmail.com>
2022-10-18 02:29:50 -0700

    Initial Commit

---
+++ Influence/InfluenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfluenceApp.swift
+//  Influence
+//
+//  Created by Ted Wu on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct InfluenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
